e8834101be268a04f6986105104b8ee474dcffcf
dailygames <noahunblockedgames@gmail.com>
2024-07-30 12:46:16 +0700

    3

--- js/main.js
+++ js/main.js
@@ -1285,7 +1285,8 @@
             34. scrollUp active
         -------------------------------------------------------- */
         $.scrollUp({
-            scrollText: '<i class="fa fa-angle-up"></i>',
+            scrollText: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path d="M12 2l-10 10h6v10h8v-10h6z"/></svg>',
+
             easingType: 'linear',
             scrollSpeed: 900,
             animation: 'fade'
